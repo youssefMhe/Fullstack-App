ff22a8ea3f38cfa790824ba98d2e845d04eb95a5
Youssef <youssef.mhenni@esprit.tn>
2020-12-01 12:19:23 +0100

    Create  all Models
    Create all models with they relations
    ( Task  [*  ---  1]  User )  relation : one to many

--- Back-End Laravel/Back/database/migrations/2020_12_01_104909_create_tasks_table.php
+++ Back-End Laravel/Back/database/migrations/2020_12_01_104909_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('topic');
+            $table->string('description');
+            $table->timestamps('date');
             $table->timestamps();
-            $table->string('role');
+            $table->bigInteger('User_id')->unsigned();;
+            $table->foreign('User_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 }
